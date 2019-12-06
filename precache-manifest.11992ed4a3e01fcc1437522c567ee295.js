efe6fb1cd35efcc82191f84037abf290630cb9ad
Vlad Slavin <vladimir.slavin@dhl.com>
2019-12-06 12:45:42 +0100

    init

---
+++ precache-manifest.11992ed4a3e01fcc1437522c567ee295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da214011f5e4ac868acf7b4f6ffe5069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a3c4a217989cc4c2d2",
+    "url": "/static/css/main.ca65f28e.chunk.css"
+  },
+  {
+    "revision": "209b99a5622929c3e836",
+    "url": "/static/js/2.c87d6864.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c87d6864.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71a3c4a217989cc4c2d2",
+    "url": "/static/js/main.223da4ff.chunk.js"
+  },
+  {
+    "revision": "6ca23e35397a24ad3cd4",
+    "url": "/static/js/runtime-main.dcf99370.js"
+  }
+]);
